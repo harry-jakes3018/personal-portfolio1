7675d3ed8f2e7166e25501b22a88a5f1aebfaa66
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-06-16 17:21:01 +0530

    Skills Section Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,44 @@
 /*=============== SHOW SIDEBAR ===============*/
 
-
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
 
-
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove("skills__active");
+        });
+
+        target.classList.add("skills__active");
+
+        tabs.forEach((tab) => {
+            tab.classList.remove("skills__active");
+        });
+        tab.classList.add("skills__active");
+    });
+});
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
 /*===== Link Active Work =====*/
-
 
 /*===== Work Popup =====*/
 
-
 /*=============== SERVICES MODAL ===============*/
-
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== INPUT ANIMATION ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SHOW SCROLL UP ===============*/
