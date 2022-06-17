a021cb310b17cb9941bfc300d417c271c0b30139
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-06-17 14:34:32 +0530

    Portfolio Popup Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,30 @@ linkWork.forEach((l) => {
 });
 
 /*===== Work Popup =====*/
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("work__button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document
+    .querySelector(".portfolio__popup-close")
+    .addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    // console.log(portfolioItem);
+    document.querySelector(".pp__thumbnail img").src =
+        portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML =
+        portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML =
+        portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
 
 /*=============== SERVICES MODAL ===============*/
 
