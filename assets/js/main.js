5c105b495efa0e7a4edd2b88edfd2f1ac1efbcd8
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-06-17 14:00:15 +0530

    Portfolio Filter Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,8 +28,28 @@ tabs.forEach((tab) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+var mixerPortfolio = mixitup(".work__container", {
+    selectors: {
+        target: ".work__card",
+    },
+    animation: {
+        duration: 300,
+    },
+});
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+    linkWork.forEach((l) => {
+        l.classList.remove("active-work");
+    });
+    this.classList.add("active-work");
+}
+
+linkWork.forEach((l) => {
+    l.addEventListener("click", activeWork);
+});
 
 /*===== Work Popup =====*/
 
