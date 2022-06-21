9b487d6f56a992c75659d5204ebad768d2806158
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-06-21 16:19:17 +0530

    Scroll Bar and Scroll Active Action Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,5 +141,27 @@ inputs.forEach((input) => {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+    let scrollY = window.pageYOffset;
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(`.nav__menu a[href*="${sectionId}"]`)
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(`.nav__menu a[href*="${sectionId}"]`)
+                .classList.remove("active-link");
+        }
+    });
+}
 
 /*=============== SHOW SCROLL UP ===============*/
